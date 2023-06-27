21e724660d8ca8ae3ec480c1fd06a901f6f4cde0
scoeg <scoeg@WANDA-OPEXCG>
2023-06-27 10:47:30 +0700

    Add project files.

---
+++ Console MES Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Camstar.WCF;
+using Camstar.WCF.ObjectStack;
+using Camstar.Util;
+using Camstar.Exceptions;
+using Camstar.WCF.Services;
+
+namespace Console_MES_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                MoveStdService oCDOService = new MoveStdService(new UserProfile("Administrator", "Ocg123!"));
+                MoveStd oCDO = new MoveStd { Container = new ContainerRef("AAA") };
+                ResultStatus oResultStatus = oCDOService.ExecuteTransaction(oCDO);
+
+                if (oResultStatus.IsSuccess)
+                {
+                    Console.WriteLine(oResultStatus.Message);
+                }
+                else throw new Exception(oResultStatus.ExceptionData.Description);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+            //Console.WriteLine("Hello World");
+            //Console.ReadKey();
+        }
+    }
+}
